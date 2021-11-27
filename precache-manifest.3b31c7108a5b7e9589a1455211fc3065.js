5301e05a3a2342039f0c2446fb68b5ae2b37f301
unknown <Leo hengebaert>
2021-11-27 13:00:03 +0100

    Updates

---
+++ precache-manifest.3b31c7108a5b7e9589a1455211fc3065.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2efe4b72b2bbd9cddc31567589cc45d4",
+    "url": "/menu-deroulant-react/index.html"
+  },
+  {
+    "revision": "a35ce46a0b7ba6a4dbd7",
+    "url": "/menu-deroulant-react/static/css/main.32f627d4.chunk.css"
+  },
+  {
+    "revision": "64e215562b34329fef95",
+    "url": "/menu-deroulant-react/static/js/2.7c61fa91.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/menu-deroulant-react/static/js/2.7c61fa91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a35ce46a0b7ba6a4dbd7",
+    "url": "/menu-deroulant-react/static/js/main.4cf8641c.chunk.js"
+  },
+  {
+    "revision": "b891ff6b8c3b0ca16635",
+    "url": "/menu-deroulant-react/static/js/runtime-main.6a4257f1.js"
+  },
+  {
+    "revision": "cd1d1c978d0030f14f1d8a7690eb8398",
+    "url": "/menu-deroulant-react/static/media/img-1.cd1d1c97.jpg"
+  },
+  {
+    "revision": "4143d240af520e4a8b565060c6e6277b",
+    "url": "/menu-deroulant-react/static/media/img-2.4143d240.jpg"
+  },
+  {
+    "revision": "65b53910f670be0b3337cb65bad3d7f6",
+    "url": "/menu-deroulant-react/static/media/img-3.65b53910.jpg"
+  },
+  {
+    "revision": "0ec923fc0fe65b0349ffbf93b33fd7c6",
+    "url": "/menu-deroulant-react/static/media/img-4.0ec923fc.jpg"
+  },
+  {
+    "revision": "c64ec5c8d3ea8c3743ca51c1fa59e50e",
+    "url": "/menu-deroulant-react/static/media/img-5.c64ec5c8.jpg"
+  },
+  {
+    "revision": "bd11375f25b4d7b599e2065c9e916507",
+    "url": "/menu-deroulant-react/static/media/img-6.bd11375f.jpg"
+  },
+  {
+    "revision": "66001cf811f9f101c450edb126e29c63",
+    "url": "/menu-deroulant-react/static/media/img-7.66001cf8.jpg"
+  }
+]);
